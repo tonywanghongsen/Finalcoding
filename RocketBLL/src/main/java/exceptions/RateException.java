d0c0b2a99b0516bd159eac18611e496724e7f022
Hongsen Wang <Hongsen Wang@10.0.0.216>
2016-12-15 23:50:15 -0500

    again

--- RocketBLL/src/main/java/exceptions/RateException.java
+++ RocketBLL/src/main/java/exceptions/RateException.java
@@ -1,9 +1,17 @@
 package exceptions;
 
+import rocketDomain.RateDomainModel;
+
 public class RateException extends Exception {
 
 	//	TODO - RocketBLL RateException - RateDomainModel should be an attribute of RateException
-	//	* Add RateRomainModel as an attribute
-	//	* Create a constructor, passing in RateDomainModel
-	//	* Create a getter (no setter, set value only in Constructor)
+	private RateDomainModel RateDo;
+	
+	public RateException(RateDomainModel Rate){
+		this.RateDo = Rate;
+	}
+	public RateDomainModel getGivenCreditScore() {
+		return this.RateDo;
+	}
+	
 }
--- RocketClient/src/main/java/rocket/app/view/MortgageController.java
+++ RocketClient/src/main/java/rocket/app/view/MortgageController.java
@@ -1,11 +1,24 @@
 package rocket.app.view;
 
+
+import java.awt.Button;
+import java.awt.TextField;
+
+import com.sun.xml.ws.org.objectweb.asm.Label;
+
 import eNums.eAction;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import rocket.app.MainApp;
 import rocketCode.Action;
 import rocketData.LoanRequest;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 
 public class MortgageController {
 
@@ -14,14 +27,36 @@ public class MortgageController {
 	//	TODO - RocketClient.RocketMainController
 	
 	//	Create private instance variables for:
-	//		TextBox  - 	txtIncome
-	//		TextBox  - 	txtExpenses
-	//		TextBox  - 	txtCreditScore
-	//		TextBox  - 	txtHouseCost
-	//		ComboBox -	loan term... 15 year or 30 year
-	//		Labels   -  various labels for the controls
-	//		Button   -  button to calculate the loan payment
-	//		Label    -  to show error messages (exception throw, payment exception)
+	//		TextBox  - 	
+	//		TextBox  - 	
+	//		TextBox  - 	
+	//		TextBox  - 	
+	//		ComboBox -	
+	//		Labels   -  
+	//		Button   -  
+	//		Label    -  
+	@FXML
+    private TextField txtIncome;
+	@FXML
+    private TextField txtExpenses;
+	@FXML
+    private TextField txtCreditScore;
+	@FXML
+    private TextField txtHouseCost;
+	@FXML
+    private TextField txtDownPayment;
+	@FXML
+    private ComboBox<Integer> cmbTerm;
+	@FXML
+    private Label lblMortgagePayment;
+
+	@FXML
+	private Label Showerrow;
+	@FXML
+	private Button Loanpayment;
+	@FXML
+	private Button btnPayment;
+	
 
 	public void setMainApp(MainApp mainApp) {
 		this.mainApp = mainApp;
@@ -30,17 +65,29 @@ public class MortgageController {
 	
 	//	TODO - RocketClient.RocketMainController
 	//			Call this when btnPayment is pressed, calculate the payment
+	
+	@FXML
+	private void initialize(){
+		cmbTerm.getItems().addAll(15,30);
+	}
 	@FXML
 	public void btnCalculatePayment(ActionEvent event)
 	{
 		Object message = null;
+		
 		//	TODO - RocketClient.RocketMainController
 		
 		Action a = new Action(eAction.CalculatePayment);
 		LoanRequest lq = new LoanRequest();
-		//	TODO - RocketClient.RocketMainController
-		//			set the loan request details...  rate, term, amount, credit score, downpayment
-		//			I've created you an instance of lq...  execute the setters in lq
+		
+		lq.setdIncome(Double.parseDouble(txtIncome.getText()));
+		lq.setdExpenses(Double.parseDouble(txtExpenses.getText()));
+		lq.setiCreditScore(Integer.parseInt(txtCreditScore.getText()));
+		lq.setiTerm((int)cmbTerm.getSelectionModel().getSelectedItem());
+		lq.setdAmount(Double.parseDouble(txtHouseCost.getText()));
+		lq.setiDownPayment(Integer.parseInt(txtDownPayment.getText()));
+
+		a.setLoanRequest(lq);
 
 		a.setLoanRequest(lq);
 		
@@ -55,6 +102,26 @@ public class MortgageController {
 		//			after it's returned back from the server, the payment (dPayment)
 		//			should be calculated.
 		//			Display dPayment on the form, rounded to two decimal places
+		double Piti;
+		if(lRequest.getdIncome()*0.36- lRequest.getdExpenses()> lRequest.getdIncome()*0.28 ){
+			Piti =  ((lRequest.getdIncome() * 0.36) - lRequest.getdExpenses());
+		}
+		else{
+			Piti = lRequest.getdIncome()*0.28;
+		}
+		double payment = lRequest.getdPayment();
+		String print;
+		print = new DecimalFormat("#.##").format(payment);
+		if(Piti>payment){
+			Showerrow.setText(" you payment will be "+ print , "Your APR is" + String.valueOf(lRequest.getdRate())+"%");
+		}else{
+			Showerrow.setText(print);
+		}
+		ObservableList<String> list = FXCollections.observableArrayList("15 Year","30 Year");
+
+
+		
+		
 		
 	}
 }
